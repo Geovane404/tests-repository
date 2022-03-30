a4dc8a80cf1ed5ac08cd77dd2d74b62b63cb007b
Geovane Calheira da Silva <geovanecalheiro@gmail.com>
2022-03-30 08:38:06 -0300

    Create TDD

---
+++ src/main/java/com/gtecnologia/tddCompleto/TddCompletoApplication.java
@@ -0,0 +1,13 @@
+package com.gtecnologia.tddCompleto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TddCompletoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TddCompletoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gtecnologia/tddCompleto/TddCompletoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gtecnologia.tddCompleto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TddCompletoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
